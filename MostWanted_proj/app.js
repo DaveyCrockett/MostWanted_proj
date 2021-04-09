99b5f361803dfbf456663b44d97b90801684e97b
DAVID paredes <augustspies22@gmail.com>
2021-04-08 17:35:19 -0700

    Added a enter table Function

--- MostWanted_proj/app.js
+++ MostWanted_proj/app.js
@@ -13,6 +13,7 @@ function searchByName(){
     // Rather than console logging, you need to append the filteredPeople to a table.
     if(filteredPeople.length > 0){
         console.log(filteredPeople);
+        enterTable(filteredPeople);
     }else{
         console.log('Sorry, looks like there is no one with that name.');
     }
@@ -21,6 +22,7 @@ function searchByName(){
 function searchByGender(){
     // Grabbing the values from our nameForm form and inputs.
     let genderInput = document.forms['searchBox']['gender'].value;
+
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
     let filteredGender = people.filter(function (person) {
         if(person.gender === genderInput){
@@ -31,48 +33,34 @@ function searchByGender(){
 });
     if(filteredGender.length > 0){
         console.log(filteredGender);
-        body = '<tbody id="byGender">'
-        for (let i = 0; i < filteredGender.length; i++) {
-            body += '<tr>';
-            body += '<td>' + filteredGender[i].firstName + '</td>';
-            body += '<td>' + filteredGender[i].lastName + '</td>';
-            body += '<td>' + filteredGender[i].gender + '</td>';
-            body += '<td>' + filteredGender[i].dob + '</td>';
-            body += '<td>' + filteredGender[i].height + '</td>';
-            body += '<td>' + filteredGender[i].weight + '</td>';
-            body += '<td>' + filteredGender[i].eyeColor + '</td>';
-            body += '<td>' + filteredGender[i].occupation + '</td>';
-            body += '<td>' + filteredGender[i].parents + '</td>';
-            body += '<td>' + filteredGender[i].currentSpouse + '</td>';
-            body += '</tr>';
-            document.getElementById('genderData').innerHTML = body;
-        }
+        enterTable(filteredGender);
+      
     }else{
         console.log('Sorry, looks like there is no one with that gender.');
     }
 }
 
-
 function searchByDOB(){
     // Grabbing the values from our nameForm form and inputs.
-    let firstNameInput = document.forms['searchBox']['dateOfBirth'].value;
+    let DobInput = document.forms['searchBox']['dateOfBirth'].value;
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
-    let filteredPeople = people.filter(function (person) {
-        if(person.firstName === firstNameInput){
+    let filteredDob = people.filter(function (person) {
+        if(person.dob === DobInput){
             return true;
         }
         return false;
     });
     
     // Rather than console logging, you need to append the filteredPeople to a table.
-    if(filteredPeople.length > 0){
-        console.log(filteredPeople);
+    if(filteredDob.length > 0){
+        console.log(filteredDob);
+        enterTable(filteredDob);
     }else{
         console.log('Sorry, looks like there is no one with that date of birth.');
     }
 }
 
-let body = '<tbody id ="fullList">'
+let body = '<tbody id="fullList">'
 for (let i = 0; i < people.length; i++) {
     body += '<tr>';
     body += '<td>' + people[i].firstName + '</td>';
@@ -89,36 +77,23 @@ for (let i = 0; i < people.length; i++) {
 }
 body +='</tbody>';
 document.getElementById('data').innerHTML = body;
-function searchByGender(){
-    // Grabbing the values from our nameForm form and inputs.
-    let genderInput = document.forms['searchBox']['gender'].value;
-    // "people" is coming from the data.js file. We have access to it within this JavaScript file.
-    let filteredGender = people.filter(function (person) {
-        if(person.gender === genderInput){
-            return true;
-        } else{
-            return false;
-        }
-});
-    if(filteredGender.length > 0){
-        console.log(filteredGender);
-        body = '<tbody id="byGender">'
-        for (let i = 0; i < filteredGender.length; i++) {
-            body += '<tr>';
-            body += '<td>' + filteredGender[i].firstName + '</td>';
-            body += '<td>' + filteredGender[i].lastName + '</td>';
-            body += '<td>' + filteredGender[i].gender + '</td>';
-            body += '<td>' + filteredGender[i].dob + '</td>';
-            body += '<td>' + filteredGender[i].height + '</td>';
-            body += '<td>' + filteredGender[i].weight + '</td>';
-            body += '<td>' + filteredGender[i].eyeColor + '</td>';
-            body += '<td>' + filteredGender[i].occupation + '</td>';
-            body += '<td>' + filteredGender[i].parents + '</td>';
-            body += '<td>' + filteredGender[i].currentSpouse + '</td>';
-            body += '</tr>';
-            document.getElementById('genderData').innerHTML = body;
-        }
-    }else{
-        console.log('Sorry, looks like there is no one with that gender.');
+
+function enterTable(searchFilter){
+    let body = '<tbody id="byDefinition">'
+    for (let i = 0; i < searchFilter.length; i++) {
+        body += '<tr>';
+        body += '<td>' + searchFilter[i].firstName + '</td>';
+        body += '<td>' + searchFilter[i].lastName + '</td>';
+        body += '<td>' + searchFilter[i].gender + '</td>';
+        body += '<td>' + searchFilter[i].dob + '</td>';
+        body += '<td>' + searchFilter[i].height + '</td>';
+        body += '<td>' + searchFilter[i].weight + '</td>';
+        body += '<td>' + searchFilter[i].eyeColor + '</td>';
+        body += '<td>' + searchFilter[i].occupation + '</td>';
+        body += '<td>' + searchFilter[i].parents + '</td>';
+        body += '<td>' + searchFilter[i].currentSpouse + '</td>';
+        body += '</tr>';
     }
+    body +='</tbody>';
+    document.getElementById('definitionData').innerHTML = body;
 }
